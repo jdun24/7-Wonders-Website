feec4f78a743ec6342cae0fcc21ffb74e83ba102
jdun24 <34287430+jdun24@users.noreply.github.com>
2019-01-04 15:50:20 -0600

    Create civ.php
    work in progress

---
+++ civ.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+        <title>Civ</title>
+
+        <style>
+
+        body {
+            background-image: url("/web/img/civ.png");
+        }
+        </style>
+        
+    </head>
+    <body>
+        <?php include("template/menuBar.php"); ?>
+    </body>
+</html>
